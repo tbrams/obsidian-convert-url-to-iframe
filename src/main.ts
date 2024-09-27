f4f407fa7d981a012497a7ceeea80f8de53c04a1
Torben Brams <torben@brams.dk>
2024-09-27 16:06:04 +0200

    First step url detection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-
 import { Editor, MarkdownView, Menu, Notice, Plugin } from 'obsidian';
 
-import { getIframe} from './utils/iframe_generator.utils';
+import { getIframe } from './utils/iframe_generator.utils';
 import { ConfigureIframeModal } from './configure_iframe_modal';
 import { isUrl } from './utils/url.utils';
 
@@ -25,7 +24,7 @@ export default class FormatNotionPlugin extends Plugin {
 			(menu: Menu, _: Editor, view: MarkdownView) => {
 				const url = this.getCleanedUrl();
 				if (url) {
-					menu.addItem((item) => {
+					menu.addItem((item) => { 
 						item.setTitle("Url to Preview/Iframe")
 							.setIcon("create-new")
 							.onClick((_) => {
@@ -34,34 +33,40 @@ export default class FormatNotionPlugin extends Plugin {
 					});
 				}
 			}));
-
 	}
 
 	async urlToIframe(inputUrl?: string): Promise<void> {
 		const activeLeaf: any = this.app.workspace.activeLeaf;
 		const editor = activeLeaf.view.sourceMode.cmEditor;
 
-		const url = inputUrl || this.getCleanedUrl()
+		const url = inputUrl || this.getCleanedUrl();
 
 		if (url) {
-			const iframeHtml = await getIframe(url)
-			const modal = new ConfigureIframeModal(this.app, iframeHtml, editor)
+			const iframeHtml = await getIframe(url);
+			const modal = new ConfigureIframeModal(this.app, iframeHtml, editor);
 			modal.open();
 		} else {
-			new Notice('Select a URL to convert to an preview/iframe.');
+			new Notice('No valid URL found at cursor position or in selection.');
 		}
 	}
 
 	private getCleanedUrl(): string {
 		const activeLeaf: any = this.app.workspace.activeLeaf;
 		const editor = activeLeaf.view.sourceMode.cmEditor;
-		const selectedText: string = editor.somethingSelected() ? editor.getSelection() : null;
-		const cleanedText = selectedText?.trim();
-
-		if (selectedText && isUrl(cleanedText)) {
-			return cleanedText
+		
+		if (editor.somethingSelected()) {
+			const selectedText: string = editor.getSelection().trim();
+			return isUrl(selectedText) ? selectedText : null;
+		} else {
+			const cursor = editor.getCursor();
+			const line = editor.getLine(cursor.line);
+			const words = line.split(/\s+/);
+			for (const word of words) {
+				if (isUrl(word) && line.indexOf(word) <= cursor.ch && line.indexOf(word) + word.length >= cursor.ch) {
+					return word;
+				}
+			}
 		}
 		return null;
 	}
-
 }
